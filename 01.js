18637244ab80d3e8eff6f1e8748df4d23b125465
unknown <tomi_877@hotmail.com>
2022-11-08 11:46:24 -0300

    ejercicio agregado

--- 01.js
+++ 01.js
@@ -11,6 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var soloEnteros = [];
+  for (var i = 0; i < array.length; i++){
+    if ( array[i] / array[i === 0]){
+      soloEnteros = array[i];
+    }
+    return soloEnteros;
+  }
 
 }
 
